77f1a8ac59045c7c5ae4d8f104f63dfaf5b01777
Yoomin Hwang <yoominh3@gmail.com>
2024-11-26 11:11:46 +0900

    Chore: Router, Styled Components, Axios installed and MainPage.js settled

---
+++ src/pages/MainPage.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+function MainPage () {
+    return (
+        <MainWrapper>
+
+        </MainWrapper>
+    );
+}
+
+export default MainPage;
+
+const MainWrapper = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: space-between;
+`
